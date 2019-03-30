109cf5fd9c412a5309f6944dea12d92f2359d9c3
James Merkin <jnmerk@gmail.com>
2019-03-30 15:04:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function destructivelyAppendKitten (element){
   }
   
   function appendKitten(element){
-    var newarray=[];
-    newarray=kittens.push(element);
+    var newarray=kittens;
+    newarray.push(element);
     return (newarray);
   }
   
