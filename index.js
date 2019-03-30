92a296d0ba83f3e4f88a707862efb220cf883fe1
James Merkin <jnmerk@gmail.com>
2019-03-30 15:15:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function destructivelyAppendKitten (element){
       return (newarray);
   }
   
-  function destructivelyPrependKittenKitten(element){
+  function PrependKittenKitten(element){
   var temparray=[element];
   var newarray=temparray.concat(kittens);
   return (temparray);
